ef7da947b1a87d6bee391ff46aa1e588aa0b26ca
Prasanth123-c <prasanthc267@gamil.com>
2023-11-16 12:31:50 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+# Create your views here.
+def macha(request):
+    return render(request,'macha.html')
+def machi(request):
+    return render(request,'machi.html')
+def macha1(request):
+    return render(request,'macha1.html')
